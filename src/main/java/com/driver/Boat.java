98884ed1c78fa442c7f2ee74c39c95d1704b8902
maheshwaram <pradeepmaheshwaram8008@gmail.com>
2025-11-04 16:16:32 +0530

    assignment

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -28,11 +28,11 @@ public class Boat implements WaterVehicle{
 
     @Override
     public String getVehicleName() {
-        return getName();
+        return this.name;
     }
 
     @Override
     public int getVehicleCapacity() {
-        return getCapacity();
+        return this.capacity;
     }
 }
